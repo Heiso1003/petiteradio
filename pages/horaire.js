981d8483b2c6ffaf83e74398bb30963fe7a11830
Debuysscher thomas <thomas.debuysscher.13037@stu.he-ferrer.eu>
2022-08-23 13:35:08 +0200

    Add Select Component

--- pages/horaire.js
+++ pages/horaire.js
@@ -1,51 +1,66 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Header from "../src/components/common/header/Header";
 import { useQuery } from "@apollo/client";
 import { getHoraire } from "../lib/wordpress/api";
-import Dropdown from "react-dropdown";
+// import Dropdown from "react-dropdown";
 import "react-dropdown/style.css";
+import Select from "react-select";
 
 export default function Horaire() {
   const { data } = useQuery(getHoraire);
+  const [day, setDay] = useState({});
 
   // IF data is undefined return empty
   if (data == undefined) {
     return;
   }
-
+  console.log(day);
   const options = [
-    "lundi",
-    "mardi",
-    "mecredi",
-    "jeudi",
-    "vendredi",
-    "samedi",
-    "dimanche",
+    { value: "lundi", label: "Lundi" },
+    { value: "mardi", label: "Mardi" },
+    { value: "mercredi", label: "Mercredi" },
+    { value: "jeudi", label: "Jeudi" },
+    { value: "vendredi", label: "Vendredi" },
+    { value: "samedi", label: "Samedi" },
+    { value: "dimanche", label: "Dimanche" },
   ];
 
-  const defaultOption = options[0];
+  // const options = [
+  //   "lundi",
+  //   "mardi",
+  //   "mercredi",
+  //   "jeudi",
+  //   "vendredi",
+  //   "samedi",
+  //   "dimanche",
+  // ];
 
-  // const [day, setDay] = useState("lundi");
+  // const defaultOption = options[0];
 
   return (
     <div>
       <Header />
       <h1 className="title">Horaire</h1>
       <div className="container">
-        <Dropdown
+        <Select
           options={options}
-          onChange={console.log('changement')}
+          placeholder="Choisir un jour..."
+          onChange={setDay}
+        />
+        {/* <Dropdown
+          options={options}
+          onChange={(e) => setDay(e.target.value)}
           value={defaultOption}
           placeholder="Choisir un jour"
-        />
+        /> */}
         <div className="row">
           {data.horaires.nodes.map((horaire) => {
-            // if (horaire.horaireContent.jour === day)
-            return (
-              <div className="col-12">
-                <h3>{horaire.horaireContent.nom}</h3>
-              </div>
-            );
+            if (horaire.horaireContent.jour === day.value)
+              return (
+                <div className="col-12">
+                  <h3>{horaire.horaireContent.nom}</h3>
+                </div>
+              );
           })}
         </div>
       </div>
